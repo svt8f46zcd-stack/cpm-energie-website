59be031392daf622100d5f4edfb1272710df4107
svt8f46zcd-stack <svt8f46zcd@privaterelay.appleid.com>
2026-09-04 00:32:27 +0200

    Fix static export for robots route

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,10 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://cpm-energie.de/sitemap.xml" };
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://cpm-energie.de/sitemap.xml",
+  };
 }
