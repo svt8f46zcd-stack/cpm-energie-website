5452d5bee6bb6c0083830044485532419cd77231
svt8f46zcd-stack <svt8f46zcd@privaterelay.appleid.com>
2026-09-10 22:08:30 +0200

    style: load isolated test and Energy Flow CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./cpm-energy-flow.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
