9912c65a0b311be145c26c0a698b2f87719716bd
svt8f46zcd-stack <svt8f46zcd@privaterelay.appleid.com>
2026-09-11 02:47:21 +0200

    fix: allow search indexing on live site

--- app/robots.ts
+++ app/robots.ts
@@ -2,15 +2,7 @@ import type { MetadataRoute } from "next";
 
 export const dynamic = "force-static";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-
 export default function robots(): MetadataRoute.Robots {
-  if (isGitHubPages) {
-    return {
-      rules: { userAgent: "*", disallow: "/" },
-    };
-  }
-
   return {
     rules: { userAgent: "*", allow: "/" },
     sitemap: "https://cpm-energie.de/sitemap.xml",
